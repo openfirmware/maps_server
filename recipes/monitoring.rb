31266b9695027440efcf8c7d060ab4a300ab8e36
James Badger <james@jamesbadger.ca>
2018-11-29 07:24:25 -0700

    Add recipe for monitoring
    Installs Munin and munin node, based on code from OpenStreetMap
    Munin cookbook:
    https://github.com/openstreetmap/chef/tree/master/cookbooks/munin

---
+++ recipes/monitoring.rb
@@ -0,0 +1,60 @@
+#
+# Cookbook:: maps_server
+# Recipe:: monitoring
+#
+# Copyright:: 2018, James Badger, Apache-2.0 License.
+# With code based on https://github.com/openstreetmap/chef/tree/master/cookbooks/munin
+
+apt_update
+
+# Install Munin Server
+package 'munin'
+package 'apache2'
+package 'rrdcached'
+package 'libcgi-fast-perl'
+package 'libapache2-mod-fcgid'
+
+template '/etc/default/rrdcached' do
+  source 'rrdcached.erb'
+  owner 'root'
+  group 'root'
+  mode '644'
+end
+
+directory "/var/lib/munin/rrdcached" do
+  owner "munin"
+  group "munin"
+  mode 0o755
+end
+
+expiry_time = 14 * 86400
+
+template "/etc/munin/munin.conf" do
+  source "munin.conf.erb"
+  owner "root"
+  group "root"
+  mode 0o644
+  variables :expiry_time => expiry_time
+end
+
+template '/etc/apache2/sites-available/munin.conf' do
+  source 'apache-munin.conf.erb'
+end
+
+%w(fcgid rewrite headers).each do |apache_module|
+  execute "enable apache module #{apache_module}" do
+    command "a2enmod #{apache_module}"
+  end
+end
+
+execute "enable munin site" do
+  command "a2ensite munin"
+end
+
+service "apache2" do
+  action :restart
+end
+
+# Install Munin Client
+package 'munin-node'
+
